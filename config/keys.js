1c1f53bda94d46195b3c31fa51152b534401a6fc
Isur <bednarczykartur96@gmail.com>
2018-12-23 19:25:26 +0100

    typo fix

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURL: process.env.NODE_ENV === 'test' ? "mongodb://localhost:27017/type-racer-test" : mongodb://typeRacer:AndroidRacer2018@ds141654.mlab.com:41654/type-racer"
+    mongoURL: process.env.NODE_ENV === 'test' ? "mongodb://localhost:27017/type-racer-test" : "mongodb://typeRacer:AndroidRacer2018@ds141654.mlab.com:41654/type-racer"
 }
