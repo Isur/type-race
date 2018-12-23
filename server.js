01aeb17d30289d40412e3c048baa2c3cc7802a60
Artur Bednarczyk <bednarczykartur96@gmail.com>
2018-12-23 19:48:09 +0100

    Setup for Client

--- server.js
+++ server.js
@@ -26,6 +26,14 @@ const api = require('./routes/api/server').router;
 
 app.use('/server', api);
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
